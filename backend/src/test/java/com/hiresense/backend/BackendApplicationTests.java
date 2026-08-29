4a8655d8b998d90c8a34c0b3f5e83ab1475e4c38
Arvind Waghmale <arvindwaghmale12@gmail.com>
2026-08-30 01:43:13 +0530

    Add Spring Boot backend setup

---
+++ backend/src/test/java/com/hiresense/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiresense.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
